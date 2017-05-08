8310faa824f8f07e342631c4903e6efd6849d559
XinWeizhou <xinyiheng518@158.com>
2017-05-08 22:37:23 +0800

    测试题叫

--- StickyMessageView/ViewController.m
+++ StickyMessageView/ViewController.m
@@ -17,8 +17,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"%@",NSHomeDirectory());
+    NSLog(@"%@",NSTemporaryDirectory());
+    [[NSBundle mainBundle]  bundlePath];
+    
+    
+    NSArray *arrDownloadPaths=NSSearchPathForDirectoriesInDomains(NSDownloadsDirectory,NSUserDomainMask,YES);
+    NSString *loadPathsPath=[arrDownloadPaths objectAtIndex:0];
+    NSLog(@"Downloads path: %@",loadPathsPath);
+    
+    
+    NSArray *arrCachesPaths=NSSearchPathForDirectoriesInDomains(NSCachesDirectory,NSUserDomainMask,YES);
+    NSString *CachesPath=[arrCachesPaths objectAtIndex:0];
+    NSLog(@"Caches path: %@",CachesPath);
+    
+    [[NSUserDefaults standardUserDefaults] setObject:@"Hello" forKey:@"Test"];
+    
 }
+// /Users/Boat/Library/Developer/CoreSimulator/Devices/74F47580-7663-4E47-9C52-B1D833B9C4D0/data/Containers/Data/Application/2C158787-3C47-482F-A1A7-EE71323B9709
 
+// /Users/Boat/Library/Developer/CoreSimulator/Devices/74F47580-7663-4E47-9C52-B1D833B9C4D0/data/Containers/Data/Application/2C158787-3C47-482F-A1A7-EE71323B9709/tmp/
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
